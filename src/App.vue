50f7a024cfcfeca4722f44b934b79ebbe77d6c0e
Ray0Emma <ahmadifarah22@gmail.com>
2021-12-24 17:23:45 +0000

    added edit option

--- src/App.vue
+++ src/App.vue
@@ -15,19 +15,25 @@
           <label>Last name</label>
           <input type="text" class="form-control" v-model="user.lastname" />
         </div>
-        <button class="btn btn-primary" @click="postData()">Add</button>
-        <br />
-        <br />
-        <button class="btn btn-primary" @click="getData()">Retrieve</button>
-        <br />
-        <br />
-        <ul class="list-group">
-          <li
-            class="list-group-item"
-            v-for="usr in users"
-            v-bind:key="usr.username"
-          >{{usr.username}} - {{usr.firstname}} {{usr.lastname}}</li>
-        </ul>
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <button class="btn btn-primary" @click="postOrPutData()">{{actionButtonTitle}}</button>
+        <button class="ms-5 btn btn-primary" @click="reset()">Reset</button>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-2">
+        <button class="btn btn-primary btn-dark" @click="getData()">Retrieve</button>
+      </div>
+    </div>
+    <div class="row" v-for="usr in users" v-bind:key="usr.username">
+      <div class="col-2">{{usr.username}}</div>
+      <div class="col-4">{{usr.firstname}} {{usr.lastname}}</div>
+      <div class="col-1">
+        <button class="btn btn-link" @click="prepareToSave(usr.id)">Edit</button>
       </div>
     </div>
   </div>
@@ -44,23 +50,39 @@ export default {
         firstname: '',
         lastname: ''
       },
-      users: []
+      users: [],
+      currentUserIdToSave: "",
+      actionButtonTitle: "Add",
     }
   },
   methods: {
-    postData() {
-      return axios
-      .post('https://vue-login-db-default-rtdb.firebaseio.com/users.json',this.user)
-      .then((response) => {
-            console.log(response.data);
+    postOrPutData() {
 
-            this.user.username = "";
-            this.user.firstname = "";
-            this.user.lastname = "";
-          })
-      .catch((errors) => {
-        console.log(errors)
-      });
+      if (this.actionButtonTitle === 'Add') {
+          return axios
+          .post('https://vue-login-db-default-rtdb.firebaseio.com/users.json',this.user)
+          .then((response) => {
+                console.log(response.data);
+
+                this.user.username = "";
+                this.user.firstname = "";
+                this.user.lastname = "";
+              })
+          .catch((errors) => {
+            console.log(errors)
+          });
+      } else {
+          return axios
+          .put('https://vue-login-db-default-rtdb.firebaseio.com/users/'+
+                this.currentUserIdToSave + ".json",
+                this.user )
+          .then((response) => {
+                console.log(response);
+              })
+          .catch((errors) => {
+            console.log(errors)
+          });     
+      }
     },
     getData() {
       return axios
@@ -72,11 +94,35 @@ export default {
           const tmpArray = [];
 
           for (let key in myData) {
+            let withId = myData[key];
+            withId.id = key;
             tmpArray.push(myData[key]);
           }
 
           this.users = tmpArray;
         });
+    },
+    prepareToSave(id) {
+      for (let i = 0; i < this.users.length; i++) {
+        if (this.users[i].id === id) {
+          this.user.username = this.users[i].username;
+          this.user.firstname = this.users[i].firstname;
+          this.user.lastname = this.users[i].lastname;
+          this.currentUserIdToSave = this.users[i].id;
+
+          this.actionButtonTitle = "Save";
+
+          break;
+        }
+      }
+    },
+    reset() {
+      this.actionButtonTitle = "Add";
+      this.currentUserIdToSave = "";
+
+      this.user.username = "";
+      this.user.firstname = "";
+      this.user.lastname = "";
     }
   },
   

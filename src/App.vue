0aa4a008064c93eabce6260dea084a8cbec962c6
Ray0Emma <ahmadifarah22@gmail.com>
2021-12-25 18:55:18 +0000

    feat: added app file and sweetalert2

--- src/App.vue
+++ src/App.vue
@@ -1,135 +1,62 @@
 <template>
-   <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h2>Vue.js Remote Server</h2>
-        <div class="form-group">
-          <label>Username</label>
-          <input type="text" class="form-control" v-model="user.username" />
-        </div>
-        <div class="form-group">
-          <label>First name</label>
-          <input type="text" class="form-control" v-model="user.firstname" />
-        </div>
-        <div class="form-group">
-          <label>Last name</label>
-          <input type="text" class="form-control" v-model="user.lastname" />
-        </div>
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <button class="btn btn-primary" @click="postOrPutData()">{{actionButtonTitle}}</button>
-        <button class="ms-5 btn btn-primary" @click="reset()">Reset</button>
-      </div>
-    </div>
-    <hr>
-    <div class="row">
-      <div class="col-2">
-        <button class="btn btn-primary btn-dark" @click="getData()">Retrieve</button>
-      </div>
-    </div>
-    <div class="row" v-for="usr in users" v-bind:key="usr.username">
-      <div class="col-2">{{usr.username}}</div>
-      <div class="col-4">{{usr.firstname}} {{usr.lastname}}</div>
-      <div class="col-1">
-        <button class="btn btn-link" @click="prepareToSave(usr.id)">Edit</button>
-      </div>
-    </div>
+  <div id="app">
+    <nav id="nav">
+      <p class="logo-place">Vue.js Remote Server (Enhanced)</p>
+      <ul class="nav-links">
+        <li class="links">
+          <router-link to="/">Home</router-link>
+        </li>
+        <li class="links">
+          <router-link to="/AddUser">Add User</router-link>
+        </li>
+        <li class="links">
+          <router-link to="/AllUsers">All User</router-link>
+        </li>
+      </ul>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 export default {
   name: 'App',
-  data () {
-    return {
-      user: {
-        username: '',
-        firstname: '',
-        lastname: ''
-      },
-      users: [],
-      currentUserIdToSave: "",
-      actionButtonTitle: "Add",
-    }
-  },
-  methods: {
-    postOrPutData() {
-
-      if (this.actionButtonTitle === 'Add') {
-          return axios
-          .post('https://vue-login-db-default-rtdb.firebaseio.com/users.json',this.user)
-          .then((response) => {
-                console.log(response.data);
-
-                this.user.username = "";
-                this.user.firstname = "";
-                this.user.lastname = "";
-              })
-          .catch((errors) => {
-            console.log(errors)
-          });
-      } else {
-          return axios
-          .put('https://vue-login-db-default-rtdb.firebaseio.com/users/'+
-                this.currentUserIdToSave + ".json",
-                this.user )
-          .then((response) => {
-                console.log(response);
-              })
-          .catch((errors) => {
-            console.log(errors)
-          });     
-      }
-    },
-    getData() {
-      return axios
-      .get('https://vue-login-db-default-rtdb.firebaseio.com/users.json')
-      .then((res) => {
-          console.log(res.data);
-
-          let myData = res.data;
-          const tmpArray = [];
-
-          for (let key in myData) {
-            let withId = myData[key];
-            withId.id = key;
-            tmpArray.push(myData[key]);
-          }
-
-          this.users = tmpArray;
-        });
-    },
-    prepareToSave(id) {
-      for (let i = 0; i < this.users.length; i++) {
-        if (this.users[i].id === id) {
-          this.user.username = this.users[i].username;
-          this.user.firstname = this.users[i].firstname;
-          this.user.lastname = this.users[i].lastname;
-          this.currentUserIdToSave = this.users[i].id;
-
-          this.actionButtonTitle = "Save";
-
-          break;
-        }
-      }
-    },
-    reset() {
-      this.actionButtonTitle = "Add";
-      this.currentUserIdToSave = "";
-
-      this.user.username = "";
-      this.user.firstname = "";
-      this.user.lastname = "";
-    }
-  },
-  
-  
+  components: {},
 }
 </script>
 
 <style>
+#nav {
+  display: flex;
+  margin-bottom: 24px;
+}
 
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  margin-right:12px;
+}
+
+#nav a.router-link-active {
+  color: #ab26ab;
+}
+
+.nav-links {
+  padding-right: 20px;
+  list-style: none;
+  display: flex;
+  margin:21px 0 0 0;
+}
+
+.links-hover {
+  text-decoration: underline;
+}
+
+.logo-place {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
+  margin:16px 0 0 16px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Swal from 'sweetalert2'
+import 'sweetalert2/src/sweetalert2.scss'
+import router from './routes'
 
 const app = createApp(App)
+
+app.use(Swal)
+app.use(router)
 
 app.mount('#app')

6c61b5bf5f43731ca9210503a9514dde06caa310
Ray0Emma <ahmadifarah22@gmail.com>
2021-12-28 18:11:22 +0000

    fix: fixed home page margin issue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="container-lg">
-    <div class="row justify-content-center mb-5">
-      <div class="col-6 text-center">
+    <div class="row justify-content-center mt-5">
+      <div class="col-6 text-center mt-4">
         <h1>Welcome in our website!</h1>
       </div>
     </div>
 
-    <div class="row justify-content-center">
-      <div class="card me-5 shadow border-0" style="width: 18rem;">
+    <div class="row justify-content-center mt-5">
+      <div class="card me-5 shadow border-0" style="width: 18rem">
         <div class="card-body">
           <h5 class="card-title">Add User</h5>
-          <p class="card-text">Add a new user to the Firebase experimental website.</p>
-          <router-link class="btn btn-dark" to="/AddUser">Add a user</router-link>
+          <p class="card-text">
+            Add a new user to the Firebase experimental website.
+          </p>
+          <router-link class="btn btn-dark" to="/AddUser"
+            >Add a user</router-link
+          >
         </div>
       </div>
 
-      <div class="card shadow border-0" style="width: 18rem;">
+      <div class="card shadow border-0" style="width: 18rem">
         <div class="card-body">
           <h5 class="card-title">All User</h5>
-          <p class="card-text">Display all users info from the Firebase experimental website.</p>
-          <router-link class="btn btn-dark" to="/AllUsers">Get all users</router-link>
+          <p class="card-text">
+            Display all users info from the Firebase experimental website.
+          </p>
+          <router-link class="btn btn-dark" to="/AllUsers"
+            >Get all users</router-link
+          >
         </div>
       </div>
     </div>
@@ -33,7 +41,7 @@ export default {
 </script>
 
 <style scoped>
-h1{
+h1 {
   color: #59b984;
 }
 </style>

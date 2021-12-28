9e13c4008952f0ae2cc8e8f7c99e5b95334db6f4
Ray0Emma <ahmadifarah22@gmail.com>
2021-12-28 10:13:12 +0000

    feat(ui): addUser page ui

--- src/pages/AddUser.vue
+++ src/pages/AddUser.vue
@@ -1,55 +1,71 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-6 offset-2">
+  <div class="container-lg">
+    <div class="row justify-content-center mt-5">
+      <div class="col-6 text-center mt-4">
         <h1>Add User</h1>
       </div>
     </div>
-    <div class="row">
-      <div class="col-6 offset-2">
+    <div class="row justify-content-center">
+      <div class="col-6">
         <div class="container">
           <div class="row">
             <div class="col-12">
               <div class="form-group">
                 <label>Username</label>
-                <input type="text" class="form-control" v-model="user.username" />
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="user.username"
+                />
               </div>
-              <div class="form-group">
+              <div class="form-group mt-3">
                 <label>First name</label>
-                <input type="text" class="form-control" v-model="user.firstname" />
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="user.firstname"
+                />
               </div>
-              <div class="form-group">
+              <div class="form-group mt-3">
                 <label>Last name</label>
-                <input type="text" class="form-control" v-model="user.lastname" />
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="user.lastname"
+                />
               </div>
             </div>
           </div>
+          <button class="btn btn-dark px-4 mt-3" @click="postUser()">
+            Add
+          </button>
         </div>
-        <button class="btn btn-primary float-left" style="margin-left:12px;" @click="postUser()">Add</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   name: "AddUser",
   methods: {
-      postUser() {
+    postUser() {
       return axios
-      .post('https://vue-login-db-default-rtdb.firebaseio.com/users.json',this.user)
-      .then(() => {
- 
-            this.user.username = "";
-            this.user.firstname = "";
-            this.user.lastname = "";
+        .post(
+          "https://vue-login-db-default-rtdb.firebaseio.com/users.json",
+          this.user
+        )
+        .then(() => {
+          this.user.username = "";
+          this.user.firstname = "";
+          this.user.lastname = "";
 
-            this.$swal("A new user is added.", "Operation Succeeded", "success");
-          })
-      .catch(() => {
-            this.$swal("Oops...", "Something went wrong!", "error");
-      });
+          this.$swal("A new user is added.", "Operation Succeeded", "success");
+        })
+        .catch(() => {
+          this.$swal("Oops...", "Something went wrong!", "error");
+        });
     },
   },
   data() {
@@ -65,7 +81,7 @@ export default {
 </script>
 
 <style scoped>
-.row {
-  margin-top: 8px;
+h1 {
+  color: #59b984;
 }
 </style>
